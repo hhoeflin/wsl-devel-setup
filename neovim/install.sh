b302d580eeea0949bf62d33e7095af61fe59cdc6
Holger Hoefling <hhoeflin@gmail.com>
2019-11-04 12:44:18 +0100

    Updated install.sh

--- neovim/install.sh
+++ neovim/install.sh
@@ -5,6 +5,9 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 # copy the initialization file for neovim to its correct location
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
+cp coc-settings.json ~/.config/nvim
+cp plugins.lua ~/.config/nvim
+
 
 # set up the powerline fonts
 # clone
